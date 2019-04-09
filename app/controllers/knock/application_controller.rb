82c75dfc9bbd5a6c0b37542067e0d8552649710b
piratechicken <simonadwyer@yahoo.com.au>
2019-04-09 16:37:08 +1000

    separated no email and wrong pw

--- app/controllers/knock/application_controller.rb
+++ app/controllers/knock/application_controller.rb
@@ -1,11 +1,16 @@
 module Knock
   class ApplicationController < ActionController::Base
     rescue_from Knock.not_found_exception_class_name, with: :not_found
+    rescue_from Knock.wrong_pw_exception_class_name, with: :wrong_pw
 
   private
 
     def not_found
-      head :not_found
+      render json: {msg: "Email not found"}, status: :not_found
+    end
+
+    def wrong_pw
+      render json: {msg: "Wrong password"}, status: :not_found
     end
   end
 end
