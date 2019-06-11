08935a6708ec453392a54f06a63c32112ebedbe0
piratechicken <simonadwyer@yahoo.com.au>
2019-06-11 20:31:24 +1000

    fix for generators required

--- lib/knock/engine.rb
+++ lib/knock/engine.rb
@@ -1,6 +1,12 @@
 module Knock
   class Engine < ::Rails::Engine
     config.eager_load_paths += Dir["#{config.root}/lib/**/"]
+    # https://github.com/nsarno/knock/issues/156
+    if ::Rails.const_defined? 'Generators'
+      config.eager_load_paths += Dir["#{config.root}/lib/**/"]
+    else
+      require_relative './authenticable'
+    end
     isolate_namespace Knock
   end
 end
